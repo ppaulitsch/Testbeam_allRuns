34baad9926cba4fde2ab4433d0d7db1214c3bc3d
jgrossma <Johannes.Grossmann@cern.ch>
2017-07-18 15:37:41 +0200

    Modify analysis walkthrough shell script

--- Complete_Analysis.sh
+++ Complete_Analysis.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Starting the analysis of the telescope Data"
 # echo "Start conversion to LCIO"
-# jobsub -c config.cfg  -csv telruns.csv  converter  1139-1145
+jobsub  -c config.cfg  -csv telruns.csv --subdir --subdirloc NAF_OUTPUT -n ./NAF/qsubparameters.cfg converter 1139-2117
 # echo "Start Clustering"				       
-# jobsub -c config.cfg  -csv telruns.csv  clustering 1139-1145
+jobsub  -c config.cfg  -csv telruns.csv --subdir --subdirloc NAF_OUTPUT -n ./NAF/qsubparameters.cfg clustering 1139-2117
 # echo "Start Hitmaker"				       
-# jobsub -c config.cfg  -csv telruns.csv  hitmaker 1139-1145
-echo "Start Hitmaker"				       
-jobsub -c config.cfg  -csv telruns.csv  aligngbl 1139-1145
+jobsub  -c config.cfg  -csv telruns.csv --subdir --subdirloc NAF_OUTPUT -n ./NAF/qsubparameters.cfg hitmaker 1139-2117
+# echo "Start Hitmaker"				       
+# jobsub  -c config.cfg  -csv telruns.csv --subdir --subdirloc NAF_OUTPUT -n ./NAF/qsubparameters.cfg aligngbl 1139-2117
